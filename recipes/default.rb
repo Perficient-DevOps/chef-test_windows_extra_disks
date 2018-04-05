402f0e3669dd3ef65dfbf3338bf35233bca37b02
Sean G Wilbur <sgwilbur@gmail.com>
2018-04-05 09:12:03 -0400

    Initial implementation of multiple disks and initializing disks.

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,15 @@
 # Recipe:: default
 #
 # Copyright:: 2018, The Authors, All Rights Reserved.
+
+# https://gist.github.com/JasonCarter80/5a9601ca1d7713929fef63ce6bf0dc6d
+powershell_script 'Initialize Drives' do
+  code <<-EOH
+  $drive = Get-Disk | Where partitionstyle -eq 'raw' | Initialize-Disk -PartitionStyle MBR
+  $drive = Get-Disk | Where NumberOfPartitions -eq 0
+  $drive |  New-Partition -DriveLetter 'F'  -Size 10MB | Format-Volume -FileSystem NTFS -NewFileSystemLabel "Data" -Confirm:$false -AllocationUnitSize 65536
+  $drive |  New-Partition -DriveLetter 'G'  -Size 10MB | Format-Volume -FileSystem NTFS -NewFileSystemLabel "Data" -Confirm:$false -AllocationUnitSize 65536
+  $drive |  New-Partition -DriveLetter 'H'  -Size 10MB | Format-Volume -FileSystem NTFS -NewFileSystemLabel "Logs" -Confirm:$false -AllocationUnitSize 65536
+  $drive |  New-Partition -DriveLetter 'I'  -Size 10MB | Format-Volume -FileSystem NTFS -NewFileSystemLabel "Temp" -Confirm:$false -AllocationUnitSize 65536
+  EOH
+end
